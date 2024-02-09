d4765b498524ebfd0c30a6a8b3e02f1f2fbca971
Matfayt <106975039+Matfayt@users.noreply.github.com>
2024-02-09 10:32:44 +0100

    update classes and setup schemas for dots

---
+++ src/server/schemas/setup-default.js
@@ -0,0 +1,215 @@
+export const thingsPresetsDefault = {
+
+  /////// y: 1
+  'dotpi-dev-011': {
+    position: { y: 1, x: 1 },
+    audioOutputType: 'T10',
+  },
+
+  'dotpi-dev-012': {
+    position: { y: 1, x: 2 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-013': {
+    position: { y: 1, x: 3 },
+    audioOutputType: 'DAEX',
+  },
+
+  'dotpi-dev-014': {
+    position: { y: 1, x: 4 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-015': {
+    position: { y: 1, x: 5 },
+    audioOutputType: 'T10',
+  },
+
+  'dotpi-dev-016': {
+    position: { y: 1, x: 6 },
+    audioOutputType: 'CRS',
+  },
+
+
+  /////// y: 2
+  'dotpi-dev-021': {
+    position: { y: 2, x: 1 },
+    audioOutputType: 'DAEX',
+  },
+
+  'dotpi-dev-022': {
+    position: { y: 2, x: 2 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-023': {
+    position: { y: 2, x: 3 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-024': {
+    position: { y: 2, x: 4 },
+    audioOutputType: 'T10',
+  },
+
+  'dotpi-dev-025': {
+    position: { y: 2, x: 5 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-026': {
+    position: { y: 2, x: 6 },
+    audioOutputType: 'DAEX',
+  },
+
+  /////// y: 3
+  'dotpi-dev-031': {
+    position: { y: 3, x: 1 },
+    audioOutputType: 'DAEX',
+  },
+
+  'dotpi-dev-032': {
+    position: { y: 3, x: 2 },
+    audioOutputType: 'T10',
+  },
+
+  'dotpi-dev-033': {
+    position: { y: 3, x: 3 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-034': {
+    position: { y: 3, x: 4 },
+    audioOutputType: 'DAEX',
+  },
+
+  'dotpi-dev-035': {
+    position: { y: 3, x: 5 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-036': {
+    position: { y: 3, x: 6 },
+    audioOutputType: 'T10',
+  },
+
+
+  /////// y: 4
+  'dotpi-dev-041': {
+    position: { y: 4, x: 1 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-042': {
+    position: { y: 4, x: 2 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-045': {
+    position: { y: 4, x: 5 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-046': {
+    position: { y: 4, x: 6 },
+    audioOutputType: 'CRS',
+  },
+
+
+  /////// y: 5
+  'dotpi-dev-051': {
+    position: { y: 5, x: 1 },
+    audioOutputType: 'DAEX',
+  },
+
+  'dotpi-dev-052': {
+    position: { y: 5, x: 2 },
+    audioOutputType: 'T10',
+  },
+
+  'dotpi-dev-053': {
+    position: { y: 5, x: 3 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-054': {
+    position: { y: 5, x: 4 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-055': {
+    position: { y: 5, x: 5 },
+    audioOutputType: 'T10',
+  },
+
+  'dotpi-dev-056': {
+    position: { y: 5, x: 6 },
+    audioOutputType: 'DAEX',
+  },
+
+
+  /////// y: 6
+  'dotpi-dev-061': {
+    position: { y: 6, x: 1 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-062': {
+    position: { y: 6, x: 2 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-063': {
+    position: { y: 6, x: 3 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-064': {
+    position: { y: 6, x: 4 },
+    audioOutputType: 'DAEX',
+  },
+
+  'dotpi-dev-065': {
+    position: { y: 6, x: 5 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-066': {
+    position: { y: 6, x: 6 },
+    audioOutputType: 'DAEX',
+  },
+
+
+  /////// y: 7
+  'dotpi-dev-071': {
+    position: { y: 7, x: 1 },
+    audioOutputType: 'T10',
+  },
+
+  'dotpi-dev-072': {
+    position: { y: 7, x: 2 },
+    audioOutputType: 'DAEX',
+  },
+
+  'dotpi-dev-073': {
+    position: { y: 7, x: 3 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-074': {
+    position: { y: 7, x: 4 },
+    audioOutputType: 'T10',
+  },
+
+  'dotpi-dev-075': {
+    position: { y: 7, x: 5 },
+    audioOutputType: 'CRS',
+  },
+
+  'dotpi-dev-076': {
+    position: { y: 7, x: 6 },
+    audioOutputType: 'T10',
+  },
+
+};
---
+++ src/server/schemas/setup.js
@@ -0,0 +1,49 @@
+import { thingsPresetsDefault } from './setup-default.js';
+
+
+export const schema = {
+  audioLookAhead: {
+    type: 'float',
+    default: 1, // in seconds
+  },
+
+  audioOutputTypes: {
+    type: 'any',
+    default: [
+      'any',
+      'DAEX', // transducer
+      'T10', // speaker
+      'CRS', // small speaker
+    ],
+  },
+
+  // in seconds
+  audioOutputTypeLatency: {
+    type: 'any',
+    default: {
+      DAEX: 0,
+      T10: 0,
+      CRS: 0,
+      MegaBoom3: 0.250,
+    },
+  },
+
+  positionLimits: {
+    type: 'any',
+    default: {
+      x: { min: -4, max: 11 },
+      y: { min: -4, max: 12 },
+    },
+  },
+
+  things: {
+    type: 'any',
+    default: {
+      ...thingsPresetsDefault,
+    },
+
+  }, // things
+
+};
+
+export const name = 'setup';
